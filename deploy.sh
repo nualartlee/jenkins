02d2ccca455aa8a40fc04b9f57dfacd83efa5c65
Javier Nualart Lee <nualartlee@gmail.com>
2019-01-16 15:47:23 -0800

    Updating deploy scripts
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes committed:
    
    	deleted:    build.sh
    	new file:   common.sh
    	new file:   deploy.sh
    	new file:   deployment_rollback.sh
    	new file:   deployment_test.sh

---
+++ deploy.sh
@@ -0,0 +1,78 @@
+#!/usr/bin/env bash
+
+#
+# Project Deployment Script
+#
+# Pulls the latest version of the current from remote origin.
+# Creates docker containers, passwords, keys, etc
+#
+# The project should already be present under git with remote origin
+# set up before running this script
+
+# Import common functions
+source ./common.sh
+
+# Print header
+clear
+echo "====================================="
+echo "           Deploy Project"
+echo
+
+# Check user is root
+check_errs $EUID "This script must be run as root"
+
+# Check if required packages are installed
+echo "Checking required packages"
+check_package pwgen
+check_package docker
+check_package docker-compose
+echo
+
+# Pull latest version from remote origin
+git pull
+check_errs $? "Unable to pull from remote repository"
+
+# Create a directory to store passwords
+if [ -e secrets ]
+then
+    echo "secrets directory already exists"
+else
+    echo "Creating secrets directory"
+    mkdir secrets
+    check_errs $? "Failed creating secrets directory"
+fi
+chmod 660 secrets
+check_errs $? "Failed setting secret directory permissions"
+
+# Create passwords
+#create_password secrets/postgres_password.txt 27
+#create_password secrets/django_secret_key.txt 47
+#create_password secrets/django_admin_pass.txt 27
+
+# Ensure docker is running
+service docker start
+check_errs $? "Failed starting docker"
+
+# Stop containers
+docker-compose down
+check_errs $? "Failed stopping containers"
+
+# Rebuild containers
+echo
+echo "Building containers"
+docker-compose build
+check_errs $? "Failed building containers"
+
+# Run containers in background
+echo
+echo "Starting containers"
+docker-compose up -d &
+check_errs $? "Failed starting containers"
+
+# Allow for startup
+sleep 5
+
+echo
+echo "Deployment Completed"
+echo "Project is running"
+echo
---
+++ deployment_test.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+#
+# Test Current Deployment
+#
+#
+
+# Import common functions
+source ./common.sh
+
+# Print header
+clear
+echo "====================================="
+echo "           Testing Deployment"
+echo
+
+# Test that nginx is serving the website
+echo "Requesting site..."
+curl -I 172.16.0.11:8080 2>&1 | egrep "HTTP.+200 OK"
+check_errs $? "Bad HTTP response when requesting the site"
+
+echo
+echo "Deployment passed all tests"
+echo "Project is running"
+echo
