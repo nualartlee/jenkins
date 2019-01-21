f05682cf2081aeb0e6e48295d50afe929121b909
Javier Nualart Lee <nualartlee@gmail.com>
2019-01-21 12:02:31 -0800

    Correcting permissions
    On branch sshkey
    Your branch is up to date with 'origin/sshkey'.
    
    Changes committed:
    
    	modified:   jenkins_container/print_public_key.sh
    	modified:   scripts/post_build.sh

--- scripts/post_build.sh
+++ scripts/post_build.sh
@@ -7,6 +7,6 @@ source scripts/common.sh
 
 echo ""
 echo "Jenkins public key:"
-docker exec jenkins_jenkins_1 /usr/share/jenkins/ref/print_public_key.sh
+docker exec -u root jenkins_jenkins_1 /usr/share/jenkins/ref/print_public_key.sh
 check_errs $? "Failed printing jenkins public key"
 echo ""
