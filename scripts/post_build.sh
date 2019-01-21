5fcdaf78f51b87df9837c7a9e8143a367b85cab0
Javier Nualart Lee <nualartlee@gmail.com>
2019-01-21 11:53:21 -0800

    Print public key script
    On branch sshkey
    Your branch is up to date with 'origin/sshkey'.
    
    Changes committed:
    
    	modified:   jenkins_container/Dockerfile
    	new file:   jenkins_container/print_public_key.sh
    	modified:   scripts/post_build.sh

--- scripts/post_build.sh
+++ scripts/post_build.sh
@@ -7,6 +7,6 @@ source scripts/common.sh
 
 echo ""
 echo "Jenkins public key:"
-docker exec jenkins_jenkins_1 cat /var/jenkins_home/.ssh/jenkins-key.pub
+docker exec jenkins_jenkins_1 /usr/share/jenkins/ref/print_public_key.sh
 check_errs $? "Failed printing jenkins public key"
 echo ""
